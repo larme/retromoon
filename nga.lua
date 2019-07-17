b7fa3ee40b48082ae58bb1cadb7c1081ccc83777
Zhao Shenyang <dev@zsy.im>
2019-07-17 18:53:33 +0800

    initial commit

---
+++ nga.lua
@@ -0,0 +1,383 @@
+local class = require('middleclass')
+
+local IMAGE_SIZE = 52488 * 16
+local ADDRESSES = 2048
+local STACK_DEPTH = 512
+local NUM_DEVICES = 0
+
+local ADDR_START = 1
+-- op mixin
+
+local OPMixin = {}
+OPMixin.static = {}
+
+function OPMixin.static:populate_op_info()
+  self.static.OP2CODE = {}
+  self.static.CODE2OP = {}
+  self.static.CODE2INST = {}
+  self.static.NUM_OPS = 0
+  for _code, op in pairs(self.OPS) do
+    self.static.NUM_OPS = self.static.NUM_OPS + 1
+    code = _code - 1
+    self.static.OP2CODE[op] = code
+    self.static.CODE2OP[code] = op
+    self.static.CODE2INST[code] = self.INSTS[op]
+  end
+end
+
+function OPMixin:exec_opcode(code)
+  local inst = self.class.CODE2INST[code]
+  inst(self)
+end
+
+function OPMixin:exec_op(op)
+  local inst = self.class.INSTS[op]
+  inst(self)
+end
+
+function OPMixin:is_valid_opcode(code)
+  -- the assumption is that opcode starts with 0 and ends with NUM_OPS - 1
+  return code >= 0 and code < self.class.NUM_OPS
+end
+
+function OPMixin:included(cls)
+  cls:populate_op_info()
+end
+
+-- nga vm
+
+-- data - STACK_DEPTH
+-- address - addresses
+-- memory - image_size
+
+local NgaVM = class('NgaVM')
+
+function NgaVM:initialize(arg)
+  raw = {}
+  rawset(self, 'raw', raw)
+  self.image_size = arg.image_size or IMAGE_SIZE
+  self.addresses = arg.addresses or ADDRESSES
+  self.stack_depth = arg.stack_depth or STACK_DEPTH
+  self.num_devices = arg.num_devices or NUM_DEVICES
+  self.addr_start = arg.addr_start or ADDR_START
+  local init_memory = arg.init_memory or false
+
+  self.io_device_handlers = arg.io_device_handlers or {}
+  self.io_query_handlers = arg.io_query_handlers or {}
+
+  -- tos, nos, tors
+  local alias_mt = {}
+
+  function alias_mt.__index(_, k)
+    if k == 'tos' then
+      return self.data[self.sp]
+    elseif k == 'nos' then
+      return self.data[self.sp - 1]
+    elseif k == 'tors' then
+      return self.address[self.rp]
+    end
+  end
+
+  function alias_mt.__newindex(_, k, v)
+    if k == 'tos' then
+      self.data[self.sp] = v
+    elseif k == 'nos' then
+      self.data[self.sp - 1] = v
+    elseif k == 'tors' then
+      self.address[self.rp] = v
+    end
+  end
+
+  self.alias = {}
+  setmetatable(self.alias, alias_mt)
+
+  -- set vm up
+  self:reset(init_memory)
+
+end
+
+-- reset/prepare
+function NgaVM:reset(init_memory)
+  if init_memory == nil then
+    init_memory = true
+  end
+
+  self.memory = {}
+  self.data = {}
+  self.address = {}
+  self.ip = self.addr_start
+  self.sp = self.addr_start
+  self.rp = self.addr_start
+
+  if init_memory then
+    self:init_mem()
+  end
+end
+
+function NgaVM:init_mem()
+  local start = self.addr_start
+
+  local nop = self.class.OP2CODE.nop
+  for i=start, self.image_size + start - 1 do
+    self.memory[i] = nop
+  end
+
+  for i=start, self.stack_depth + start - 1 do
+    self.data[i] = nop
+  end
+
+  for i=start, self.addresses + start - 1 do
+    self.address[i] = nop
+  end
+end
+
+function NgaVM:print_stack()
+  io.stdout:write("stack:\t")
+  for i=self.addr_start,self.sp do
+    io.stdout:write(tostring(self.data[i]))
+    io.stdout:write("\t")
+  end
+  io.stdout:write("\n")
+end
+
+function NgaVM:load_image(path)
+  -- each line is a cell
+  local f = assert(io.open(path))
+  idx = self.addr_start
+  for line in io.lines(path) do
+    cell = tonumber(line)
+    self.memory[idx] = cell
+    idx = idx + 1
+  end
+  assert(f:close())
+end
+
+function NgaVM:validate_packed_opcodes(raw_code)
+    local current
+    local valid = -1
+    for i=0,3 do
+    current = raw_code & 0xFF
+    if not self:is_valid_opcode(current) then
+      valid = 0
+      break
+    end
+    raw_code = raw_code >> 8
+  end
+  return valid
+end
+
+function NgaVM:exec_packed_opcodes(raw_code)
+  for i=0,3 do
+    self:exec_opcode(raw_code & 0xFF)
+    raw_code = raw_code >> 8
+  end
+end
+
+-- ops
+
+NgaVM.static.OPS = {
+  'nop',
+  'lit', 'dup', 'drop', 'swap', 'push', 'pop',
+  'jump', 'call', 'ccall', 'return_', 'eq', 'neq',
+  'lt', 'gt', 'fetch', 'store', 'add', 'sub', 'mul', 'divmod',
+  'and_', 'or_', 'xor', 'shift', 'zret', 'end_',
+  'io_enum', 'io_query', 'io_interact',
+}
+
+local _insts = {}
+NgaVM.static.INSTS = _insts
+
+function _insts.nop(vm)
+  return
+end
+
+function _insts.lit(vm)
+  vm.sp = vm.sp + 1
+  vm.ip = vm.ip + 1
+  vm.alias.tos = vm.memory[vm.ip]
+end
+
+function _insts.dup(vm)
+  vm.sp = vm.sp + 1
+  vm.alias.tos = vm.alias.nos
+  return
+end
+
+function _insts.drop(vm)
+  vm.data[vm.sp] = 0
+  vm.sp = vm.sp - 1
+  if vm.sp < vm.addr_start then
+    vm.ip = vm.image_size
+  end
+end
+
+function _insts.swap(vm)
+  vm.alias.tos, vm.alias.nos = vm.alias.nos, vm.alias.tos
+end
+
+function _insts.push(vm)
+  vm.rp = vm.rp + 1
+  vm.alias.tors = vm.alias.tos
+  vm:exec_op('drop')
+end
+
+function _insts.pop(vm)
+  vm.sp = vm.sp + 1
+  vm.alias.tos = vm.alias.tors
+  vm.rp = vm.rp - 1
+end
+
+function _insts.jump(vm)
+  vm.ip = vm.alias.tos - 1
+  vm:exec_op('drop')
+end
+
+function _insts.call(vm)
+  vm.rp = vm.rp + 1
+  vm.alias.tors = vm.ip
+  vm.ip = vm.alias.tos - 1
+  vm:exec_op('drop')
+end
+
+function _insts.ccall(vm)
+  local addr, flag
+  addr = vm.alias.tos
+  vm:exec_op('drop')
+  flag = vm.alias.tos
+  vm:exec_op('drop')
+
+  if flag ~= 0 then
+    vm.rp = vm.rp + 1
+    vm.alias.tors = vm.ip
+    vm.ip = addr - 1
+  end
+end
+
+function _insts.return_(vm)
+  vm.ip = vm.alias.tors
+  vm.rp = vm.rp - 1
+end
+
+function _insts.eq(vm)
+  vm.alias.nos = (vm.alias.nos == vm.alias.tos) and -1 or 0
+  vm:exec_op('drop')
+end
+
+function _insts.neq(vm)
+  vm.alias.nos = (vm.alias.nos ~= vm.alias.tos) and -1 or 0
+  vm:exec_op('drop')
+end
+
+function _insts.lt(vm)
+  vm.alias.nos = (vm.alias.nos < vm.alias.tos) and -1 or 0
+  vm:exec_op('drop')
+end
+
+function _insts.gt(vm)
+  vm.alias.nos = (vm.alias.nos > vm.alias.tos) and -1 or 0
+  vm:exec_op('drop')
+end
+
+function _insts.fetch(vm)
+  if vm.alias.tos >= vm.addr_start then
+    vm.alias.tos = vm.memory[vm.alias.tos]
+  elseif vm.alias.tos == -1 then
+    vm.alias.tos = vm.sp - vm.addr_start - 1
+  elseif vm.alias.tos == -2 then
+    vm.alias.tos = vm.rp
+  elseif vm.alias.tos == -3 then
+    vm.alias.tos = vm.image_size
+  end
+end
+
+function _insts.store(vm)
+  vm.memory[vm.alias.tos] = vm.alias.nos
+  vm:exec_op('drop')
+  vm:exec_op('drop')
+end
+
+function _insts.add(vm)
+  vm.alias.nos = vm.alias.nos + vm.alias.tos
+  vm:exec_op('drop')
+end
+
+function _insts.sub(vm)
+  vm.alias.nos = vm.alias.nos - vm.alias.tos
+  vm:exec_op('drop')
+end
+
+function _insts.mul(vm)
+  vm.alias.nos = vm.alias.nos * vm.alias.tos
+  vm:exec_op('drop')
+end
+
+function _insts.divmod(vm)
+  local a, b = vm.alias.tos, vm.alias.nos
+  vm.alias.tos = b // a
+  vm.alias.nos = b % a
+end
+
+function _insts.and_(vm)
+  vm.alias.nos = vm.alias.tos & vm.alias.nos
+  vm:exec_op('drop')
+end
+
+function _insts.or_(vm)
+  vm.alias.nos = vm.alias.tos | vm.alias.nos
+  vm:exec_op('drop')
+end
+
+function _insts.xor(vm)
+  vm.alias.nos = vm.alias.tos ~ vm.alias.nos
+  vm:exec_op('drop')
+end
+
+function _insts.shift(vm)
+  local x, y
+  y = vm.alias.tos
+  x = vm.alias.nos
+
+  if y < 0 then
+    vm.alias.nos = x << (-1 * y)
+  elseif y > 0 then
+    if x < 0 then
+      vm.alias.nos = x >> y | ~(~0 >> y)
+    else
+      vm.alias.nos = x >> y
+    end
+  end
+  vm:exec_op('drop')
+end
+
+function _insts.zret(vm)
+  if vm.alias.tos == 0 then 
+    vm:exec_op('drop')
+    vm.ip = vm.alias.tors
+    vm.rp = vm.rp - 1
+  end
+end
+
+function _insts.end_(vm)
+  vm.ip = vm.image_size
+end
+
+function _insts.io_enum(vm)
+  vm.sp = vm.sp + 1
+  vm.alias.tos = vm.num_devices
+end
+
+function _insts.io_query(vm)
+  local device = vm.alias.tos
+  vm:exec_op('drop')
+  vm.io_query_handlers[device](vm)
+end
+
+function _insts.io_interact(vm)
+  local device = vm.alias.tos
+  vm:exec_op('drop')
+  vm.io_device_handlers[device](vm)
+end
+
+NgaVM:include(OPMixin)
+
+return NgaVM
---
+++ test_nga.lua
@@ -0,0 +1,167 @@
+require("profiler")
+local class = require('middleclass')
+local NgaVM = require('nga2')
+
+local TestNgaVM = class('TestNgaVM', NgaVM)
+
+function TestNgaVM:find_entry(named)
+  local header = self.memory[2]
+  local done = false
+  while header ~= 0 and not done do
+    if named == self:extract_string(header + 3) then
+      done = true
+    else
+      header = self.memory[header]
+    end
+  end
+  return header
+end
+
+function TestNgaVM:extract_string(at)
+  local offset = 0
+  local buffer = {}
+  while true do
+    local ch = self.memory[at + offset]
+    if ch == 0 then
+      return table.concat(buffer)
+    end
+    offset = offset + 1
+    buffer[offset] = string.char(ch)
+  end
+end
+
+function TestNgaVM:inject_string(s, to)
+  for i=1, #s do
+    ch = s:sub(i, i)
+    self.memory[to + i - 1] = string.byte(ch)
+  end
+  self.memory[to + #s] = 0
+end
+
+function TestNgaVM:exec(entry)
+  self.ip = entry
+  self.rp = self.rp + 1
+  self.alias.tors = 0
+  local notfound = self.memory[self:find_entry('err:notfound') + 1]
+  while self.ip < 100000 and self.rp > 0 do
+    -- print("ip: ", self.ip)
+    if self.ip == notfound then
+      print("ERROR: word not found!")
+    end
+
+    local raw_code = self.memory[self.ip]
+
+    if self:validate_packed_opcodes(raw_code) then
+      self:exec_packed_opcodes(raw_code)
+    else
+      print("Invalid Bytecode", raw_code, self.ip)
+      self.ip = 2000000
+    end
+    self.ip = self.ip + 1
+  end
+end
+
+local io_query_handlers = {}
+
+io_query_handlers[1] = function(vm)
+  vm.sp = vm.sp + 1
+  vm.alias.tos = 0
+  vm.alias.nos = 0
+end
+
+local io_device_handlers = {}
+
+local function disp_char(vm)
+  local char = vm.alias.tos
+  if char > 0 and char < 128 then
+    local c = string.char(char)
+    io.stdout:write(c)
+    if char == 8 then
+      io.stdout:write(string.char)
+    end
+  end
+  vm.exec_op('drop')
+  io.stdout:flush()
+end
+
+io_device_handlers[1] = disp_char
+
+function split(s, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t={}
+  for str in string.gmatch(s, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
+function run()
+  local vm = TestNgaVM{
+    addr_start = 0,
+    io_device_handlers = io_device_handlers,
+    io_query_handlers = io_query_handlers,
+  }
+  vm:load_image('image.txt')
+
+  local done = false
+  interpreter_addr = vm:find_entry('interpret') + 1
+  interpreter = vm.memory[interpreter_addr]
+
+  while not done do
+    for i=vm.addr_start, vm.sp do
+      io.stdout:write(tostring(vm.data[i]) .. ' ')
+    end
+    print("\nOK> ")
+    local line = io.stdin:read()
+    if line == 'bye' then
+      done = true
+    else
+      for _, token in ipairs(split(line)) do
+	vm:inject_string(token, 1025)
+	vm.sp = vm.sp + 1
+	vm.alias.tos = 1025
+	vm:exec(interpreter)
+      end
+    end
+  end
+end
+
+function run_profiler()
+  local vm = TestNgaVM{
+    addr_start = 0,
+    io_device_handlers = io_device_handlers,
+    io_query_handlers = io_query_handlers,
+  }
+  vm:load_image('image.txt')
+
+  local done = false
+  interpreter_addr = vm:find_entry('interpret') + 1
+  interpreter = vm.memory[interpreter_addr]
+
+  profilerStart()
+  for i=1,1 do
+    print(i)
+    for i=vm.addr_start, vm.sp do
+      io.stdout:write(tostring(vm.data[i]) .. ' ')
+    end
+    io.stdout:write("\n")
+    -- local line = '#1 #5 dup dup + dup #1 #5 dup dup + dup #1 #5 dup dup + dup #1 #5 dup dup + dup #1 #5 dup dup + dup #1 #5 dup dup + dup #1 #5 dup dup + dup #1 #5 dup dup + dup'
+    local line = '#1 #5 dup dup + dup'
+    for _, token in ipairs(split(line)) do
+      print(token)
+      vm:inject_string(token, 1025)
+      vm.sp = vm.sp + 1
+      vm.alias.tos = 1025
+      vm:exec(interpreter)
+    end
+  end
+  profilerStop()
+  profilerReport("profiler.log")
+end
+
+-- run_profiler()
+run()
+
+return TestNgaVM
